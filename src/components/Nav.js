28163da5cee30793f114cbacae54d553e1fca9cc
pdavisDU <pdavisdu@gmail.com>
2023-09-28 13:42:36 -0500

    setting up placeholder text for the navbar, centering the title text required tricker

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="menu">Home Organizer</div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Nav;
