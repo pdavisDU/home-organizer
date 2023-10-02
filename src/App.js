55c55e0544a1094ae857273689bb06343b42f550
pdavisDU <pdavisdu@gmail.com>
2023-10-02 15:02:27 -0500

    finally fixing navbar centering and link issues. Set up link for navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
 import Homepage from "./components/Homepage";
+import CategoriesPage from "./components/CategoriesPage";
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Homepage />} />
+        <Route path="/categories" exact element={<CategoriesPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <div className="nav">
-      <div className="menu">Home Organizer</div>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-      </ul>
-    </div>
+    <nav className="navbar">
+      <div className="left-container"></div>
+      <div className="nav-title">Home Organizer</div>
+      <div className="right-container">
+        <button className="hamburger" onClick={() => setShowMenu(!showMenu)}>
+          &#9776;
+        </button>
+        <ul className={`nav-links ${showMenu ? "active" : ""}`}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/categories">Categories</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
